2877ea55760e69e61daa660fc6139c1e54df6bc0
simocos37 <simonecosentino2001@libero.it>
2025-10-05 11:52:22 +0200

    Add basic rate limiting & security headers

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -7,8 +7,17 @@ import eventsRouter from './routes/events';
 import bookingsRouter from './routes/bookings';
 import mapPostRouter from './routes/mappoints';
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
+app.use(helmet());
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: true,
+  legacyHeaders: false,
+}));
 setupSwagger(app);
 const prisma = new PrismaClient();
 
